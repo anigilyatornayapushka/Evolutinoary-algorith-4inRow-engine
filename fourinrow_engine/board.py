65f79f67cdc2b350472d81e78e3114680d8d01f4
anigilyatornayapushka <zinchenkobogdan123@gmail.com>
2025-12-28 03:54:20 +0300

    Fix errors, DRY principle, Single Responsibility

--- fourinrow_engine/board.py
+++ fourinrow_engine/board.py
@@ -6,8 +6,8 @@ from enum import Enum
 from copy import deepcopy
 
 from .exceptions import (
-    FullColumnException,
-    LastMoveDoesNotExistsException,
+    OutOfRangeError,
+    NoLastMoveError,
 )
 
 
@@ -38,7 +38,7 @@ class Board:
         self._rows = rows
         self._columns = columns
         self._mv_order = mv_order
-        self.board = [[] for _ in range(columns)]
+        self._board = [[] for _ in range(columns)]
 
 
     def _swtch_mv_order(self) -> None:
@@ -46,28 +46,26 @@ class Board:
 
 
     def _validate_move(self, column: int) -> None:
-        """Raises `LastMoveDoesNotExistsException()` if invalid move."""
-        if (column < 0
-                or column >= self._columns
-                or self._board[column].__len__ == self._rows):
-            raise LastMoveDoesNotExistsException()
+        """Raises `OutOfRangeError()` if invalid move."""
+        if not 0 <= column < self._columns or len(self._board[column]) == self._rows:
+            raise OutOfRangeError()
 
 
-    def _h(self, column: int) -> int:
-        """Return index of the last element in the column."""
-        return self._board.get(column).__len__ - 1
+    @property
+    def board(self) -> List[List[Side]]:
+        return deepcopy(self._board)
 
 
-    def do_move(self, column: int) -> int:
+    def do_move(self, column: int) -> None:
         """
         Simply add a piece on the board depending of whos turn to move.
 
         Throws:
-            - LastMoveDoesNotExistsException()
+            - OutOfRangeError()
         """
         try:
             self._validate_move(column)
-        except LastMoveDoesNotExistsException as err:
+        except OutOfRangeError as err:
             raise err
 
         self._lst_mv = column
@@ -80,13 +78,14 @@ class Board:
         Cancles last move in the board.
 
         Throws:
-            - FullColumnException()
+            - NoLastMoveError()
         """
-        if not self._lst_mv:
-            raise FullColumnException()
+        if self._lst_mv is None:
+            raise NoLastMoveError()
 
         self._board[self._lst_mv].pop()
         self._swtch_mv_order()
+        self._lst_mv = None
 
 
     def get_state_by_last_move(self) -> GameState:
@@ -94,105 +93,116 @@ class Board:
         Returns state of the game calculating position around last move.
 
         Throws:
-            - FullColumnException()
+            - NoLastMoveError()
         """
-        if not self._lst_mv:
-            raise FullColumnException()
+        state_checker = StateChecker(
+            self._rows, self._columns, self.board)
+        try:
+            game_state = state_checker.get_state_by_last_move(
+                self._mv_order, self._lst_mv)
+        except NoLastMoveError as err:
+            raise err
 
-        height: int = self._h(self._lst_mv)
-        width: int = self._lst_mv
+        return game_state
 
-        # VERTICAL CHECK
-        if (height >= 3 and  # Not less than 4 pieces in the column
-                self._board[-1] == self._board[-2] ==
-                self._board[-3] == self._board[-4]):
-            if self._mv_order == Side.A:
-                # Since side was switched after a move
-                return GameState.PLAYER_2_WIN
-            return GameState.PLAYER_1_WIN
 
-        # HORIZONTAL CHECK
-        counter = 1  # Counts same pieces in neighbourhood
+class StateChecker:
+    """Check current state of `Board` depending of last move."""
+    _rows: int
+    _cols: int
+    _height: int
+    _width: int
+    _mv_order: Side
+    _board: List[List[Side]]
 
-        for delta in range(-1, -4, -1):  # Checking left side from -1 to -3
-            col: int = width + delta
-            if (col < 0 or self._h(col) < height or
-                    self._board[col][height]
-                    != self._board[width][height]):
+    def __init__(self, rows: int, columns: int,
+                 board: List[List[Side]],
+                 lst_mv: Optional[int],
+                 mv_order: Side) -> None:
+        """
+        Throws:
+            - NoLastMoveError()
+        """
+        self._rows = rows
+        self._cols = columns
+        self._board = board
+        self._mv_order = mv_order
+
+        if lst_mv is None:
+            raise NoLastMoveError()
+
+        self._height: int = self._h(lst_mv)
+        self._width: int = lst_mv
+
+
+    def __counter(self, rng: range, *, dx: int = 1, dy: int = 0) -> int:
+        """
+        rng = range(start, end, step) -
+        param for iteration in neighbourhood
+
+        dx, dy are responsible for direction of shifting
+        They are either 1 or -1 or 0 if not use.
+        """
+        counter = 0
+        for delta in rng:
+            col: int = self._width + delta * dx
+            row: int = self._height + delta * dy
+            if (col < 0 or row < 0 or col >= self._cols
+                    or row >= self._rows or self._h(col) < row
+                    or self._board[col][row]
+                    != self._board[self._width][self._height]):
                 break
             counter += 1
+        return counter
 
-        for delta in range(1, 4):  # Checking right side from +1 to +3
-            col: int = width + delta
-            if (col >= self._columns or self._h(col) < height or
-                    self._board[col][height]
-                    != self._board[width][height]):
-                break
-            counter += 1
 
+    def __define_winner(self, counter: int) -> Optional[GameState]:
+        """Define who won (if any) by move order."""
         if counter >= 4:
             if self._mv_order == Side.A:
                 # Since side was switched after a move
                 return GameState.PLAYER_2_WIN
             return GameState.PLAYER_1_WIN
+        return None
 
-        # DIAGONAL CHECK
-        counter = 1
-        # Checking / diagonal
-        for delta in range(-1, -4, -1):  # Checking left side from -1 to -3
-            row: int = height + delta
-            col: int = width + delta
-            if (col < 0 or row < 0 or self._h(col) < row or
-                    self._board[col][row]
-                    != self._board[width][height]):
-                break
-            counter += 1
 
-        for delta in range(1, 3):  # Checking right side from +1 to +3
-            row: int = height + delta
-            col: int = width + delta
-            if (col >= self._columns or row >= self._rows
-                    or self._h(col) < row or
-                    self._board[col][row]
-                    != self._board[width][height]):
-                break
-            counter += 1
+    def _h(self, column: int) -> int:
+        """Return index of the last element
+        in the column or -1 for empty."""
+        return len(self._board[column]) - 1
 
-        if counter >= 4:
-            if self._mv_order == Side.A:
-                # Since side was switched after a move
-                return GameState.PLAYER_2_WIN
-            return GameState.PLAYER_1_WIN
 
-        counter = 1
-        # Checking \ diagonal
-        for delta in range(-1, -4, -1):  # Checking left side from -1 to -3
-            row: int = height - delta
-            col: int = width + delta
-            if (col < 0 or row < 0 or self._h(col) < row or
-                    self._board[col][row]
-                    != self._board[width][height]):
-                break
-            counter += 1
+    def _vert_check(self) -> Optional[GameState]:
+        counter = (1 + self.__counter(range(1,4), dx=0, dy=-1))
+        return self.__define_winner(counter)
 
-        for delta in range(1, 3):  # Checking right side from +1 to +3
-            row: int = height - delta
-            col: int = width + delta
-            if (col >= self._columns or row >= self._rows
-                    or self._h(col) < row or
-                    self._board[col][row]
-                    != self._board[width][height]):
-                break
-            counter += 1
 
-        if counter >= 4:
-            if self._mv_order == Side.A:
-                # Since side was switched after a move
-                return GameState.PLAYER_2_WIN
-            return GameState.PLAYER_1_WIN
+    def _horiz_check(self) -> Optional[GameState]:
+        counter = (1 + self.__counter(range(-1, -4, -1))
+                   + self.__counter(range(1,4)))
+        return self.__define_winner(counter)
 
-        return GameState.RUNNING
 
-    @property
-    def board(self) -> List[List[int]]:
-        return deepcopy(self._board)
+    def _diag_check(self) -> Optional[GameState]:
+        counter1 = (1 + self.__counter(range(-1, -4, -1), dx=1, dy=1)
+                    + self.__counter(range(1,4), dx=1, dy=1))
+        counter2 = (1 + self.__counter(range(-1, -4, -1), dx=1, dy=-1)
+                    + self.__counter(range(1,4), dx=1, dy=-1))
+        if res := self.__define_winner(counter1):
+            return res
+        return self.__define_winner(counter2)
+
+
+    def get_state_by_last_move(self) -> GameState:
+        """
+        Returns state of the game
+        calculating position around last move.
+        """
+        check1: Optional[GameState] = self._vert_check()
+        check2: Optional[GameState] = self._horiz_check()
+        check3: Optional[GameState] = self._diag_check()
+        check4: Optional[GameState] = None
+        if all(len(col) == self._rows for col in self._board):
+            check4 = GameState.DRAW
+
+        return check1 or check2 or check3 or check4 or GameState.RUNNING
--- fourinrow_engine/exceptions.py
+++ fourinrow_engine/exceptions.py
@@ -1,8 +1,8 @@
-class FullColumnException(Exception):
+class OutOfRangeError(Exception):
     def __init__(self):
         super().__init__('Overflow of one of the board columns.')
 
 
-class LastMoveDoesNotExistsException(Exception):
+class NoLastMoveError(Exception):
     def __init__(self):
         super().__init__('Cannot cancel move if there are none of them.')
